ef487cbd3e6ab77e47864774d5eb58a4d9265674
metinkaya <metinkaya1511@gmail.com>
2020-05-02 04:05:18 -0400

    update

---
+++ HelloWorlds.java
@@ -0,0 +1,10 @@
+
+public class HelloWorlds{
+	public static void main(String... args){ //public static void main(String [] args)
+		System.out.println(args.length);
+			for(String a : args){
+				System.out.println(a);
+		}
+	}
+}
+
